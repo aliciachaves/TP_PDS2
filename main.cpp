7851afca580fe307d6ffb3a2cb7d10bc5a5cbc81
ClaraTv21 <132157377+ClaraTv21@users.noreply.github.com>
2023-06-20 15:39:02 -0300

    indexacao

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <map>
 #include <windows.h>
 
@@ -8,20 +9,21 @@
 std::string normalizarPalavra(const std::string& palavra) {
     std::string palavraNormalizada;
     for (char c : palavra) {
-            palavraNormalizada += std::tolower(c);
+        palavraNormalizada += std::tolower(c);
     }
     return palavraNormalizada;
 }
 
-std::map<std::string, std::string> lerArquivosDaPasta(const std::string& pasta) {
-    std::map<std::string, std::string> conteudos;
+std::map<std::string, std::map<std::string, int>> lerArquivosDaPasta(const std::string& pasta) {
+
+    std::map<std::string, std::map<std::string, int>> ocorrencias;
 
     WIN32_FIND_DATAA findData;
     HANDLE hFind = FindFirstFileA((pasta + "\\*").c_str(), &findData);
 
     if (hFind == INVALID_HANDLE_VALUE) {
         std::cerr << "Erro ao abrir a pasta." << std::endl;
-        return conteudos;
+        return ocorrencias;
     }
 
     do {
@@ -44,7 +46,7 @@ std::map<std::string, std::string> lerArquivosDaPasta(const std::string& pasta) 
             std::string palavra;
 
             for (char c : conteudo) {
-                if (std::isalpha(c) || c == ' ' || c == '\n') {
+                if (std::isalpha(c) || c == ' ' || c == '\n') { // é preciso a condição de espaço e quebra de linha?
                     palavra += std::tolower(c);
                 }
                 else if (!std::isalpha(c)) {
@@ -52,6 +54,7 @@ std::map<std::string, std::string> lerArquivosDaPasta(const std::string& pasta) 
                 }
                 else if (!palavra.empty()) {
                     conteudoNormalizado += normalizarPalavra(palavra) + " ";
+
                     palavra.clear();
                 }
             }
@@ -60,7 +63,31 @@ std::map<std::string, std::string> lerArquivosDaPasta(const std::string& pasta) 
                 conteudoNormalizado += normalizarPalavra(palavra) + " ";
             }
 
-            conteudos.insert(std::pair<std::string, std::string>(nomeArquivo, conteudoNormalizado));
+            //std::string str = normalizarPalavra(palavra);
+            std::istringstream iss(conteudoNormalizado);
+
+            do {
+                std::string word;
+                iss >> word;
+
+                if (ocorrencias.empty()) {
+                    ocorrencias.insert(std::make_pair(word, std::map<std::string, int>{ {nomeArquivo, 1} }));
+                }
+                else {
+                    bool found = false;
+                    for (auto& ocorrencia : ocorrencias) {
+                        if (ocorrencia.first == word) {
+                            ocorrencia.second[nomeArquivo]++;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        ocorrencias.insert(std::make_pair(word, std::map<std::string, int>{ {nomeArquivo, 1}}));
+                    }
+                }
+
+            } while (iss);
 
             arquivo.close();
         }
@@ -71,18 +98,27 @@ std::map<std::string, std::string> lerArquivosDaPasta(const std::string& pasta) 
 
     FindClose(hFind);
 
-    return conteudos;
+    return ocorrencias;
 }
 
 int main() {
-    std::string pasta = "C:\\Users\\alici\\Documents\\documentos_teste_tp2";
+    std::string pasta = "C:\\Users\\Clara Garcia\\OneDrive\\Documentos\\documentos_teste_tp2";
 
-    std::map<std::string, std::string> conteudos = lerArquivosDaPasta(pasta);
+    std::map<std::string, std::map<std::string, int>> ocorrencias = lerArquivosDaPasta(pasta);
 
-    for (const auto& conteudo : conteudos) {
-        std::cout << "Nome do Arquivo: " << conteudo.first << "\nConteudo:\n" << conteudo.second << std::endl;
+    for (const auto& palavra : ocorrencias) {
+        if (palavra.first != "\n") {
+            std::cout << "\n" << palavra.first;
+            for (const auto& arquivo : palavra.second) {
+                if (palavra.second.empty()) {
+                 std::cout << " (" << arquivo.first << ", " << arquivo.second << ")";
+                }
+                else {
+                    std::cout << ", (" << arquivo.first << ", " << arquivo.second << ")";
+                }
+            }
+        }
     }
 
     return 0;
 }
-
