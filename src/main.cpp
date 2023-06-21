efc13423fed4fb32a4652062959111869a5a024c
ClaraTv21 <clara21ufmg@gmail.com>
2023-06-21 10:41:21 -0300

    busca

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,19 @@
 
 
 int main() {
-    
+    std::string folder = "/home/clara21gt/TP_PDS2/documentos_teste_tp2";
+
+    ReadFile read;
+
+    std::map<std::string, std::map<std::string, int>> frequency = read.readFromFolder(folder);
+
+    Search s;
+
+    s.readWords();
+    std::vector<std::pair<std::string, int>> orderedVec = s.returnFiles(frequency);
+    for(const auto& pair : orderedVec){
+            std::cout << pair.first << std::endl;
+    }
 
     return 0;
 }
--- src/search.cpp
+++ src/search.cpp
@@ -14,4 +14,36 @@ void Search::normalizeSearch(){
     for (int i = 0; i < words.size(); i++){
         words[i] = n.normalizeContent(words[i]);
     }
+
+bool compareByValue(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+    return a.second > b.second;
+}
+
+
+std::vector<std::pair<std::string, int>> Search::returnFiles(std::map<std::string, std::map<std::string, int>> frequency) {
     
+    this->words.normalizeSearch();
+
+    std::map<std::string, File> files;
+
+    for (const auto& word : this->words) {
+        for (const auto& f : frequency[word]) {
+            files[f.first].hits += f.second;
+            files[f.first].freq++;
+            files[f.first].nome = f.first;
+        }
+    }
+
+    int len = this->words.size();
+    std::map<std::string, int> repeatedFiles;
+    for (const auto& file : files) {
+        if (file.second.freq == len) {
+            repeatedFiles[file.first] = files[file.first].hits;
+        }
+    }
+
+    std::vector<std::pair<std::string, int>> sortedVector(repeatedFiles.begin(), repeatedFiles.end());
+    std::sort(sortedVector.begin(), sortedVector.end(), compareByValue);
+
+    return sortedVector;
+}
