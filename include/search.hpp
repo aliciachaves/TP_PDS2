dbb2af0212395e2c9254dccf51b1a6144ef288d4
aliciachaves <aliciamarchaves@gmail.com>
2023-06-20 21:54:33 -0300

    searchh

--- include/search.hpp
+++ include/search.hpp
@@ -6,11 +6,18 @@
 #include <map>
 #include <vector>
 
+struct File{
+    std::string nome;
+    int freq;
+    int hits;    
+};
+
 
 class Search{
     public:
         void normalizeSearch();
         void readWords();
+        std::vector<std::pair<std::string, int>> returnFiles(std::map<std::string, std::map<std::string, int>> frequency);
     
     private:
         std::vector <std::string> words;
